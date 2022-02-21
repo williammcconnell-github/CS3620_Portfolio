f8536dc0147d2483ad8a0604ed371ce426d17e96
William McConnell <william.mcconnell0@gmail.com>
2022-02-21 14:23:42 -0700

    Added Views
    Added the Home, Hobbies, Portfolio and Contact views. Home and Contact have information being returned in the view.

---
+++ portfoliosite/PortfolioDatabase/urls.py
@@ -0,0 +1,9 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('home/', views.home, name="home"),
+    path('hobbies/', views.hobbies, name="hobbies"),
+    path('portfolio/', views.portfolio, name="portfolio"),
+    path('contact/', views.contact, name="contact")
+]
--- portfoliosite/PortfolioDatabase/views.py
+++ portfoliosite/PortfolioDatabase/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def home(request):
+    return HttpResponse("<h1>Home</h1>"
+                        "<p>Welcome to my home page</p>"
+                        "<p>My name is William McConnell. I am a 29 year old CS student looking to graduate and move "
+                        "into the work force as of the end of this spring.</p>")
+
+
+def hobbies(request):
+    return HttpResponse("")
+
+
+def portfolio(request):
+    return HttpResponse("")
+
+
+def contact(request):
+    return HttpResponse("williammcconnell@mail.wsu.edu")
