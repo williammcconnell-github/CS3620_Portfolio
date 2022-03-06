3ae2dedbfe409d9f9e5d57eba072b8e7f846e4e6
William McConnell <37432398+williammcconnell-github@users.noreply.github.com>
2022-03-06 14:12:51 -0700

    Admin & Database Updates
    Updated admin file to add models
    Updated gitignore to include and retain database updates

--- portfoliosite/PortfolioDatabase/models.py
+++ portfoliosite/PortfolioDatabase/models.py
@@ -4,10 +4,18 @@ from django.db import models
 
 
 class Hobby(models.Model):
+
+    def __str__(self):
+        return self.hobby_name
+
     hobby_name = models.CharField(max_length=200)
     hobby_desc = models.CharField(max_length=200)
 
 
 class Portfolio(models.Model):
+
+    def __str__(self):
+        return self.portfolio_name
+
     portfolio_name = models.CharField(max_length=200)
     portfolio_desc = models.CharField(max_length=200)
